022058c24b9cd764ece45e9e778de7e98b9848f4
SAMMY <119563245+acheampong-tech@users.noreply.github.com>
2023-07-05 23:38:43 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "web" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t3.micro"
+  count = "2"
 
   tags = {
-    Name = "demo-vpc"
+    Name = "DEMO-TERRAFROM"
     Purpose = "Jenkins Demo"
   }
 }
